d7c1ea5ab38f505b9b4e56399b9d29bed475eec0
yusefrich <yusef.rick@gmail.com>
2018-12-12 10:20:27 -0300

    consumed from a api

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -15,8 +15,11 @@ def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
     # 1. Add a line of code here to decode JSON from the response.
-
-    return "My name is {} {} and the PIN on my card is {}.".format(
+    name = requests.get("http://uinames.com/api?ext&region=Italy")
+    namej = name.json()
+    return "My name is {} {} and the PIN on my card is {}.".format(namej["name"],
+                                                                   namej["surname"],
+                                                                   namej["credit_card"]["pin"]
         # 2. Add the correct fields from the JSON data structure.
     )
 
