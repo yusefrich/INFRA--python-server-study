2fbf6b7b7fdfc047f05a0eadb8c8d8b7d0d2b617
yusefrich <yusef.rick@gmail.com>
2018-12-12 10:46:17 -0300

    URI shortner

--- Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -75,7 +75,14 @@ def CheckURI(uri, timeout=5):
     (i.e. times out).
     '''
     # 1. Write this function.  Delete the following line.
-    raise NotImplementedError("Step 1 isn't written yet.")
+    try:
+        r = requests.get(uri, timeout=timeout)
+        # If the GET request returns, was it a 200 OK?
+        if r.status_code == 200:
+            return True
+    except requests.RequestException:
+        # If the GET request raised an exception, it's not OK.
+        return False
 
 
 class Shortener(http.server.BaseHTTPRequestHandler):
@@ -87,8 +94,10 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         if name:
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
-                #    Delete the following line.
-                raise NotImplementedError("Step 2 isn't written yet.")
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -114,8 +123,10 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         # Check that the user submitted the form fields.
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Please submit valid parameters.")
 
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
@@ -125,14 +136,18 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             memory[shortname] = longuri
 
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+
         else:
             # Didn't successfully fetch the long URI.
 
             # 5. Send a 404 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response(404)
+            self.send_header('content-type', 'text/plain: charset=utf-8')
+            self.end_headers()
+            self.wfile.write("page not found error 404")
 
 if __name__ == '__main__':
     server_address = ('', 8000)
